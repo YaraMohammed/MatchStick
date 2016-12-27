d7e016be561b43bd530de9924a8df4b3e283f8c7
yaraMohammed <Yara.mohamed1@h-eng.helwan.edu.eg>
2016-12-27 17:26:02 +0200

    draw equation Added

--- js/eq.js
+++ js/eq.js
@@ -1,6 +1,6 @@
 divequation();
+draw_equation(7,"+",0,6);
 var im = document.getElementsByClassName("strickimgs");
-//console.log(im)
 var existim0 = false;
 
 //Add viewing to matchstick
@@ -10,19 +10,107 @@ for(var i=0;i<im.length;i++)
     e.target.classList.toggle('view');
   });
 }
+
 //Create Images in div
 function divequation(){
 var div = document.getElementById("divequation");
 for(var i=0 ; i<13 ; i++)
 {
-  //console.log(i);
     div.innerHTML+='<img src="images/vertical.png" alt="" id=img'+i+' class ="strickimgs">'
-    //  div.appendChild()
 }
 for(var i=16 ; i<28 ; i++)
 {
-  //console.log(i);
     div.innerHTML+='<img src="images/horizontal.png" alt="" id=img'+i+' class ="strickimgs">'
-    //  div.appendChild()
 }
+}
+
+//equation draw
+function draw_equation(f,op,s,r)
+{
+	//getting IDs for first operand , second operand and result in equation
+	var first = set_number(f,"first");
+	var sec = set_number(s,"sec");
+	var res = set_number(r,"res");
+	//first operand
+	for(i=0;i<first.length;i++)
+	{
+		var image = document.getElementById("img"+first[i]);
+		image.classList.add('view');
+	}
+	//second operand
+		for(i=0;i<sec.length;i++)
+	{
+		var image = document.getElementById("img"+sec[i]);
+		image.classList.add('view');
+	}
+	//result
+		for(i=0;i<res.length;i++)
+	{
+		var image = document.getElementById("img"+res[i]);
+		image.classList.add('view');
+	}
+
+	//drawing =
+	for(i=23;i<=24;i++)
+	{
+		var image = document.getElementById("img"+i);
+		image.setAttribute("class" , "view");
+	}
+
+	//drawing operand
+	var image = document.getElementById("img19");
+	image.setAttribute("class" , "view");
+	if(op=="+")
+	{
+		var image2 = document.getElementById("img4");
+		image2.classList.add('view');
+	}
+}
+
+function set_number(num,pos){
+	var temp = [];
+	switch(pos)
+	{
+		case 'first':
+			if(num==0){temp = [0,1,2,3,17,18];}
+			else if(num==1){temp = [0,1];}
+			else if(num==2){temp = [1,2,16,17,18];}
+			else if(num==3){temp = [2,3,16,17,18];}
+			else if(num==4){temp = [0,2,3,16];}
+			else if(num==5){temp = [0,3,16,17,18];}
+			else if(num==6){temp = [0,1,3,16,17,18];}
+			else if(num==7){temp = [2,3,17];}
+			else if(num==8){temp = [0,1,2,3,16,17,18];}
+			else if(num==9){temp = [0,2,3,16,17,18];}
+				return temp;
+				break;
+		case "sec":
+			if(num==0){temp = [5,6,7,8,21,22]}
+			else if(num==1){temp = [5,6]}
+			else if(num==2){temp = [6,7,20,21,22]}
+			else if(num==3){temp = [7,8,20,21,22];}
+			else if(num==4){temp = [5,7,8,20];}
+			else if(num==5){temp = [5,8,20,21,22];}
+			else if(num==6){temp = [5,8,20,21,22];}
+			else if(num==7){temp = [7,8,21];}
+			else if(num==8){temp = [5,6,7,8,20,21,22];}
+			else if(num==9){temp = [5,7,8,20,21,22];}
+				return temp;
+				break;
+		case "res":
+			if(num==0){temp = [9,10,11,12,26,27]}
+			else if(num==1){temp = [9,10]}
+			else if(num==2){temp = [10,11,25,26,27]}
+			else if(num==3){temp = [11,12,25,26,27];}
+			else if(num==4){temp = [9,11,12,25];}
+			else if(num==5){temp = [9,12,25,26,27];}
+			else if(num==6){temp = [9,10,12,25,26,27];}
+			else if(num==7){temp = [11,12,26];}
+			else if(num==8){temp = [9,10,11,12,25,26,27];}
+			else if(num==9){temp = [9,11,12,25,26,27];}
+				return temp;
+				break;
+		default:
+			console.log("error getting IDs for number " +num+ "in " +pos);
+	}
 }
