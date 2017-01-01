1d85a47d868b4846d5ea1a9e853cfecf26df63b8
sayed24 <sayedelabd04@gmail.com>
2017-01-01 18:34:53 +0200

    Update equations.js

--- js/equations.js
+++ js/equations.js
@@ -2,8 +2,14 @@ var equations = {
 	//level 1
 1: {"first":8, "sec":0, "op":"+" , "res":9 , "mode":"remove" , "sol":["img1"]},
 2: {"first":3, "sec":1, "op":"+" , "res":2 , "mode":"remove" , "sol":["img4"]},
-3: {"first":0, "sec":1, "op":"+" , "res":9 , "mode":"add" , "sol":["img16"]},
-4: {"first":4, "sec":4, "op":"+" , "res":9 , "mode":"add" , "sol":["img10"]}
+3: {"first":0, "sec":1, "op":"+" , "res":9 , "mode":"add" , "sol":["img16"]}
 	//level 2
+4: {"first":5, "sec":8, "op":"+" , "res":8 , "mode":"remove" ,"sol": ["img5","img6"]},
+5: {"first":1, "sec":1, "op":"-" , "res":3 , "mode":"add" ,"sol": ["img0","img16"]},
+6: {"first":8, "sec":9, "op":"+" , "res":4 , "mode":"remove" ,"sol": ["img21","img22"]},
+//level 3
+7: {"first":8, "sec":7, "op":"+" , "res":8 , "mode":"remove" ,"sol": ["img21","img10"]},
+8: {"first":6, "sec":7, "op":"-" , "res":7 , "mode":"remove" ,"sol": ["img1","img26"]},
+9: {"first":8, "sec":7, "op":"+" , "res":2 , "mode":"remove" ,"sol": ["img1","img4"]}
 
 };
